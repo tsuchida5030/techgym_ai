855aca4785e90336179537ba8e1c086da10b80fb
k-tsuchida <k-tsuchida@tanaka-iron-works.com>
2021-04-07 21:31:00 +0900

    2021-4-7 進捗分

---
+++ Chapter_0/Answer_sheet/3rd/AI_0-13_3_e4Ht-Qk8a.py
@@ -0,0 +1,41 @@
+#AI-TECHGYM-N-13
+
+import pandas as pd
+import urllib
+
+#Seaborn(表示のために読み込み)
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+import os
+os.chdir('C:/Users/tsuchida/Documents/techgym_セミナー/TortoiseGit_resorce/techgym_ai/Chapter_0/Answer_sheet/AI_Chapter0_saved_files')
+
+data = "http://archive.ics.uci.edu/ml/machine-learning-databases/balloons/adult+stretch.data"
+balloons = pd.read_csv(data)
+
+#indexを説明からつける
+columns_name = ['color','size','act','age','inflated']
+balloons.columns = columns_name
+
+#表示確認
+#display(balloons)
+
+#カテゴリーごとの集計
+pd.crosstab(balloons['size'], balloons['color'])
+
+#グラフの大きさを指定
+plt.figure(figsize=(10, 10))
+
+#colorのヒストグラムを表示
+plt.subplot(2,2,1)
+sns.countplot(x='color', data=balloons)
+
+#colorカテゴリごとのage件数
+plt.subplot(2,2,2)
+sns.countplot(x='color', data=balloons, hue='age', hue_order=['CHILD','ADULT'])
+
+#colorカテゴリごとのsize件数
+plt.subplot(2,2,3)
+sns.countplot(x='color', data=balloons, hue='size', hue_order=['SMALL','LARGE'])
