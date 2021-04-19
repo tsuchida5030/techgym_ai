fe1f15345564e4d113945fcd94c0692a5261e689
k-tsuchida <k-tsuchida@tanaka-iron-works.com>
2021-04-19 15:05:28 +0900

    20210416 進捗分

---
+++ Chapter_0/Answer_sheet/5th/AI_0-14_5_j4Zs-j2Y8.py
@@ -0,0 +1,45 @@
+#AI-TECHGYM-N-14
+
+import pandas as pd
+import urllib
+
+#Seaborn
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+import os
+os.chdir('C:/Users/tsuchida/Documents/techgym_セミナー/TortoiseGit_resorce/techgym_ai/Chapter_0/Answer_sheet/AI_Chapter0_saved_files')
+
+#データの取得
+data = "https://archive.ics.uci.edu/ml/machine-learning-databases/tic-tac-toe/tic-tac-toe.data"
+TTT = pd.read_csv(data,header=None)
+
+txt= "https://archive.ics.uci.edu/ml/machine-learning-databases/tic-tac-toe/tic-tac-toe.names"
+urllib.request.urlretrieve(txt, './tic-tac-toe.names')
+
+#説明文の表示(必要であれば表示)
+#f = open("./tic-tac-toe.names","r")
+#for line in f:
+#    print(line)
+#f.close()
+
+#データの個数や型を確認
+TTT.info()
+
+#indexを説明からつける
+columns_name = ['top-left','top-middle','top-right',
+                'middle-left','middle-middle','middle-right',
+                'bottom-left','bottom-middle','bottom-right','P-N']
+TTT.columns = columns_name
+
+#必要であれば表示して確認
+#display(TTT)
+display(pd.crosstab(TTT['middle-middle'], TTT['P-N']))
+
+# グラフの大きさを指定
+plt.figure(figsize=(10, 10))
+
+#真ん中をとったときの調査
+sns.countplot(x='P-N',hue='middle-middle',hue_order=['x','o','b'],data=TTT)
