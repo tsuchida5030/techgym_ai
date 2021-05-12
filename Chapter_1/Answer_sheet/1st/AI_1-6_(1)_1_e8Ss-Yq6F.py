e6d0959ba7ad0a12a403e5df324be61e41fd4d09
k-tsuchida <k-tsuchida@tanaka-iron-works.com>
2021-05-12 22:07:02 +0900

    2021/5/20,21 進捗分

---
+++ Chapter_1/Answer_sheet/1st/AI_1-6_(1)_1_e8Ss-Yq6F.py
@@ -0,0 +1,45 @@
+﻿#AI-TECHGYM-1-6-Q-1
+#教師なし学習 PCA
+
+#データ加工・処理・分析ライブラリ
+import pandas as pd
+
+#可視化ライブラリ
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+%matplotlib inline
+
+#インポート
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+#乳がんデータを読み込むためのインポート
+from sklearn.datasets import load_breast_cancer
+
+#乳がんデータの取得
+cancer = load_breast_cancer()
+
+# display(cancer)
+
+X = pd.DataFrame(cancer['data'], columns=cancer['feature_names'])
+
+# Figureを追加
+fig = plt.figure(figsize = (8, 8))
+
+# 3DAxesを追加
+ax = fig.add_subplot(111, projection='3d')
+
+# 軸ラベルを設定
+ax.set_xlabel("mean radius", size = 14, color = "r")
+ax.set_ylabel("mean texture", size = 14, color = "r")
+ax.set_zlabel("mean perimeter", size = 14, color = "r")
+
+ax.scatter(X["mean radius"], X["mean texture"], X["mean perimeter"], s=20, c='black')
+
+sc = StandardScaler()
+X_std = sc.fit_transform(X)
+
+pca = PCA(n_components=3)
+pca.fit(X_std.iloc[:, 0:3])
+
+print(pca.shape_)
---
+++ Chapter_1/Answer_sheet/1st/AI_1-6_(2)_1_e8Ss-T3JE.py
@@ -0,0 +1,54 @@
+﻿#AI-TECHGYM-1-6-Q-1
+#教師なし学習 PCA
+
+#データ加工・処理・分析ライブラリ
+import pandas as pd
+
+#可視化ライブラリ
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+%matplotlib inline
+
+#インポート
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+#アヤメのデータを読み込むためのインポート
+from sklearn.datasets import load_iris
+
+#サンプルデータ
+iris = load_iris()
+
+print('iris.data shape:{}'.format(iris.data.shape))
+
+fig = plt.figure(figsize = (8, 8))
+ax_3D = fig.add_subplot(111, projection='3d')
+ax_3D.set_xlabel(iris['feature_names'][1], size = 14, color = "r")
+ax_3D.set_ylabel(iris['feature_names'][2], size = 14, color = "r")
+ax_3D.set_zlabel(iris['feature_names'][3], size = 14, color = "r")
+ax_3D.view_init(elev=10., azim=-15)
+ax_3D.scatter(iris['data'][:,1], iris['data'][:,2], iris['data'][:,3], s=20, c='black')
+
+sc = StandardScaler()
+sc.fit(iris.data)
+X_std = sc.transform(iris.data)
+
+pca = PCA(n_components=2)
+pca.fit(X_std)
+X_pca = pca.transform(X_std)
+
+print('X_pca shape:{}'.format(X_pca.shape))
+
+X_pca = pd.DataFrame(X_pca, columns=['pc1', 'pc2'])
+
+X_pca = pd.concat([X_pca, pd.DataFrame(iris.target, columns=['target'])], axis=1)
+
+#悪性、良性を分ける
+pca_setosa = X_pca[X_pca['target']==0]
+pca_versicolor = X_pca[X_pca['target']==1]
+pca_virginica = X_pca[X_pca['target']==2]
+
+#悪性、良性をプロット
+ax = pca_setosa.plot.scatter(x='pc1', y='pc2', color='blue', label='setosa');
+pca_versicolor.plot.scatter(x='pc1', y='pc2', color='red', label='versicolor', ax=ax);
+pca_virginica.plot.scatter(x='pc1', y='pc2', color='green', label='virginica', ax=ax);
