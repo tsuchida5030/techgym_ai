9a7eb0a0ab881f6c3b14d5cf05e96b4859aeb23d
k-tsuchida <k-tsuchida@tanaka-iron-works.com>
2021-05-18 22:16:16 +0900

    2021/5/17,18進捗分

---
+++ Chapter_1/Answer_sheet/1st/AI_1-8_(1)_1_jB65-fT5j.py
@@ -0,0 +1,13 @@
+﻿#AI-TECHGYM-1-8-Q
+#自然言語処理
+
+from janome.tokenizer import Tokenizer
+
+tokenizer = Tokenizer()
+
+sentence = 'すもももももももものうち'
+
+print(sentence)
+
+for token in tokenizer.tokenize(sentence):
+  print("    " + str(token))
---
+++ Chapter_1/Answer_sheet/1st/AI_1-8_(3)_1_jB65-Ct2p.py
@@ -0,0 +1,22 @@
+﻿#AI-TECHGYM-1-8-Q
+#自然言語処理
+
+from janome.tokenizer import Tokenizer
+
+import os
+os.chdir("C:/Users/tsuchida/Documents/techgym_セミナー/TortoiseGit_resorce/techgym_ai/Chapter_1/Answer_sheet/AI_Chapter1_saved_files")
+
+f = open('techgym-AI.txt')
+s = f.read()
+print(s)
+print()
+print('以下、形態素解析の結果')
+print('**************************************')
+
+t = Tokenizer()
+tokens = t.tokenize(s)
+
+for token in tokens:
+  print(token)
+
+f.close()
