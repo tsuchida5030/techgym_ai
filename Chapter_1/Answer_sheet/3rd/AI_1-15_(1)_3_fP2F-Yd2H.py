259514517073ba60199d2bbcd720a8914ec0ef6b
k-tsuchida <k-tsuchida@tanaka-iron-works.com>
2022-02-08 21:00:50 +0900

    2020-02-08 進捗分

---
+++ Chapter_1/Answer_sheet/3rd/AI_1-15_(1)_3_fP2F-Yd2H.py
@@ -0,0 +1,25 @@
+﻿#AI-TECHGYM-1-15-Q-1
+#自然言語処理
+
+#インポート
+from gensim.models import KeyedVectors
+import os
+import numpy as np
+import pandas as pd
+from gensim.models import word2vec
+
+os.chdir(r"C:\Users\tsuchida\Documents\techgym_セミナー\TortoiseGit_resorce\techgym_ai\Chapter_1\Answer_sheet\AI_Chapter1_saved_files")
+
+# ダウンロード先のパスを指定
+MODEL_FILENAME = "./stanby-jobs-200d-word2vector.bin"
+w2v = KeyedVectors.load_word2vec_format(MODEL_FILENAME, binary=True)
+
+print("単語を入力してください。\n")
+word = input()
+
+try:
+  sim_word = w2v.most_similar(word)[0][0]
+  print('{0}といったら{1}です。'.format(word, sim_word) + '\n')
+  print('{}と言ったら？'.format(sim_word))
+except:
+  print('モデルにない単語なのでもう一度入力してください。')
