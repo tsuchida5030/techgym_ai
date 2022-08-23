002e4498a9ecb45cc0e26a57392ada236af09400
k-tsuchida <k-tsuchida@tanaka-iron-works.com>
2022-08-23 21:21:09 +0900

    2022-08-23_進捗分

--- Chapter_1/Answer_sheet/5th/AI_1-8_(4)_5_jB65-u6QN.py
+++ Chapter_1/Answer_sheet/5th/AI_1-8_(4)_5_jB65-u6QN.py
@@ -1,6 +1,10 @@
 ﻿#AI-TECHGYM-1-8-A-3
 #自然言語処理
 
+#ファイル格納場所
+import os
+os.chdir(r"C:\Users\tsuchida\Documents\techgym_セミナー\TortoiseGit_resorce\techgym_ai\Chapter_1\Answer_sheet\AI_Chapter1_saved_files")
+
 from janome.tokenizer import Tokenizer
 
 #形態素解析のオブジェクト
@@ -9,13 +13,15 @@ text = Tokenizer()
 #txtファイルからデータの読み込み
 text_file = open("techgym-AI.txt")
 txt = text_file.read()
- 
+
+lists_c = []
 #読み込んだデータを形態素解析
 lines = txt.split("\n")
 for i in lines:
-  print(i)
-  print("\n")
-  text_c = text.tokenize(i)    
-  for j in text_c:
-    print(j)
-  print("\n")
+  # print(i)
+  # print("\n")
+  list_c = text.tokenize(i, wakati=True)
+  lists_c.extend(list_c)
+  # print("\n")
+
+print(lists_c)
