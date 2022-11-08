a2831868cb7c71fea024e7d01ee9c36725440a5b
k-tsuchida <k-tsuchida@tanaka-iron-works.com>
2022-11-08 20:50:27 +0900

    2022-11-08 進捗分

---
+++ Chapter_1/Answer_sheet/5th/AI_1-15_(1)_5_fP2F-Yd2H.py
@@ -0,0 +1,21 @@
+﻿#AI-TECHGYM-1-15-Q-1
+#自然言語処理
+
+#インポート
+from gensim.models import KeyedVectors
+import os
+
+os.chdir(r"C:\Users\tsuchida\Documents\techgym_セミナー\TortoiseGit_resorce\techgym_ai\Chapter_1\Answer_sheet\AI_Chapter1_saved_files")
+
+# ダウンロード先のパスを指定
+MODEL_FILENAME = "./stanby-jobs-200d-word2vector.bin"
+w2v = KeyedVectors.load_word2vec_format(MODEL_FILENAME, binary=True)
+
+print("単語を入力してください。\n")
+word = input()
+
+try:
+  similar = w2v.most_similar(word)
+  print(similar[0])
+except:
+  print("指定された単語はありません")
---
+++ Chapter_1/Answer_sheet/5th/AI_1-15_(2)_5_fP2F-W3ms.py
@@ -0,0 +1,32 @@
+﻿#AI-TECHGYM-1-15-Q-1
+#自然言語処理
+
+#インポート
+from gensim.models import KeyedVectors
+import os
+
+os.chdir(r"C:\Users\tsuchida\Documents\techgym_セミナー\TortoiseGit_resorce\techgym_ai\Chapter_1\Answer_sheet\AI_Chapter1_saved_files")
+
+# ダウンロード先のパスを指定
+MODEL_FILENAME = "./stanby-jobs-200d-word2vector.bin"
+w2v = KeyedVectors.load_word2vec_format(MODEL_FILENAME, binary=True)
+
+print("連想ゲームのはじめの単語を入力してください。\n")
+
+i = 1
+while True:
+  word = input()
+  if word == "Q":
+    break
+
+  try:
+    job = w2v.most_similar(word,topn=1)
+    hantei = True
+  except :
+    hantei = False
+
+  if hantei == True:
+    print('\'{1}\'といったら\'{2}\'です。\n'.format(word, job[0][0]))
+    print('\'{}\'といったら？\n'.format(job[0][0]))
+  else:
+    print("モデルにない単語なのでもう一度入力してください。\n")
