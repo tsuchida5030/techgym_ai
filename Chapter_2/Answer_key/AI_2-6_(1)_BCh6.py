194d7e04afcf6833477183777da743af6f2f4750
k-tsuchida <k-tsuchida@tanaka-iron-works.com>
2023-01-10 20:12:29 +0900

    2022-01-10 進捗分

--- Chapter_2/Answer_key/AI_2-6_(1)_BCh6.py
+++ Chapter_2/Answer_key/AI_2-6_(1)_BCh6.py
@@ -5,17 +5,19 @@
 import pandas as pd
 
 #読み込みデータ
-pg_data = {'0':['Python'],'1':['Ruby'],'2':['PHP'],'3':['Java'],'4':['JavaScript']}
-df_row = pd.DataFrame(pg_data)
-df = df_row.T
+pg_data = {'0':['Python'],'1':['Ruby'],'2':['PHP'],'3':['Java'],'4':['JavaScript']} #項目名の辞書型
+df_row = pd.DataFrame(pg_data)                                                      #項目名が一行目に横に並ぶデータフレーム
+df = df_row.T                                                                       #項目名が一列目に縦に並ぶデータフレーム
 #display(df)
 
 #one hot encoding
-df_c = pd.get_dummies(df)
+df_c = pd.get_dummies(df)                                                           #カテゴリ変数となる項目名が一列目に縦に並ぶデータフレームに対して、ワンホットエンコーディングを実行し、カテゴリ変数の行列を作る
+#出力されるデータフレームの列名は、<元の列名"0">_<カテゴリ変数名>となる
+#get_dummiesのオプション・・・prefix, prefix_sep, columns
 
 #表示用並び替え
-df_d = df_c.iloc[:,[3,4,2,0,1]]
-display(df_d)
+df_d = df_c.iloc[:,[3,4,2,0,1]]                                                     #ワンホットエンコーディングしたデータフレームの列順を、1が対角項に来るよう並び変える
+display(df_d)                                                                       #1が対角項に並んだ、カテゴリ変数の行列
 
 #columnを設定して、値を取得
 new_df = pd.DataFrame(columns=["row", "vector"])
