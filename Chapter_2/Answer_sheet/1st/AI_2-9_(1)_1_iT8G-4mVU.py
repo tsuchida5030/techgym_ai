abe7cc0c272589557ed9d4d9fa405f2148b67d50
k-tsuchida <k-tsuchida@tanaka-iron-works.com>
2023-04-18 21:07:24 +0900

    2023-04-18 進捗分

---
+++ Chapter_2/Answer_sheet/1st/AI_2-9_(1)_1_iT8G-4mVU.py
@@ -0,0 +1,37 @@
+#AI-TECHGYM-2-9-Q
+#特徴量エンジニアリング
+
+# 実行場所
+import os
+os.chdir(r"C:\Users\tsuchida\Documents\techgym_セミナー\TortoiseGit_resorce\techgym_ai\Chapter_2\Answer_sheet\AI_Chapter2_saved_files")
+
+#インポート
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+
+#ファイルがなければ前の問題を実施する
+title = "FIFA_data_pre.csv"
+if not os.path.exists(title):
+    print("Run Previous problem.")
+else :
+    print(title + " EXIST.")
+    df=pd.read_csv('./FIFA_data_pre.csv')
+
+st_time = time.time()
+
+#スタイルの指定
+plt.style.use('fivethirtyeight')
+sns.set(style = "dark", palette = "colorblind", color_codes = True)
+plt.figure(figsize = (15,5))
+plt.tick_params(labelsize = 16)
+
+#必要に応じて表示
+#display(df.head())
+#display(df.columns)
+
+en_time = time.time()
+wst_time = en_time - st_time
+print(wst_time)
