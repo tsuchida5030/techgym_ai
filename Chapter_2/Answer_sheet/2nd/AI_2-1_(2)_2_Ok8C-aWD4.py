3832e4919bab11c54bcc77771410bf551475ff2f
tsuchida5030 <k-tsuchida@tanaka-iron-works.com>
2023-11-14 21:55:53 +0900

    2023-11-14_進捗

--- Chapter_2/Answer_sheet/2nd/AI_2-1_(2)_2_Ok8C-aWD4.py
+++ Chapter_2/Answer_sheet/2nd/AI_2-1_(2)_2_Ok8C-aWD4.py
@@ -4,6 +4,7 @@
 #インポート
 import pandas as pd
 import matplotlib.pyplot as plt 
+import seaborn as sns
 
 #乳がんデータを読み込むためのインポート
 from sklearn.datasets import load_breast_cancer
@@ -14,5 +15,9 @@ cancer = load_breast_cancer()
 #データフレーム
 df = pd.DataFrame(data=cancer.data , columns=cancer.feature_names)
 
-plt.hist(df["mean radius"], bins=16)
+sns.set()
+sns.set_style('whitegrid')
+
+sns.displot(data=df, x="mean radius", bins=16, kde=True)
+plt.legend()
 plt.show()
