44c668061ebb3729988dafa0a7d84d3797cde592
tsuchida5030 <k-tsuchida@tanaka-iron-works.com>
2024-05-13 14:25:51 +0900

    2024-05-13_同期

--- Chapter_2/Answer_sheet/2nd/AI_2-1_(2)_2_Ok8C-aWD4.py
+++ Chapter_2/Answer_sheet/2nd/AI_2-1_(2)_2_Ok8C-aWD4.py
@@ -1,6 +1,10 @@
 #AI-TECHGYM-2-1-Q
 #特徴量エンジニアリング
 
+# 作業場所
+import os
+os.chdir(r"C:\Users\tsuchida\Documents\techgym_セミナー\tortoisegit\techgym_ai\Chapter_2\Answer_sheet\AI_Chapter2_saved_files")
+
 #インポート
 import pandas as pd
 import matplotlib.pyplot as plt 
@@ -18,6 +22,5 @@ df = pd.DataFrame(data=cancer.data , columns=cancer.feature_names)
 sns.set()
 sns.set_style('whitegrid')
 
-sns.displot(data=df, x="mean radius", bins=16, kde=True)
-plt.legend()
+sns.displot(data=df, x="mean radius", bins=16, kde=True, legend=True)
 plt.show()
